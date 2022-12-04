1004be2df026372064022dd9c11b4c03680efbd5
Trapti88 <113841534+Trapti88@users.noreply.github.com>
2022-12-04 21:21:16 +0530

    Add files via upload

---
+++ SourceCode.py
@@ -0,0 +1,74 @@
+# Project -  Phishing Detector with LR
+
+# Author - TRAPTI MESHRAM
+# Date - 12/04/2022
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+phis = pd.read_csv('C:/Phishing/phishing.txt')
+X = phis.iloc[:,:-1].values
+y = phis.iloc[:,30].values
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=4)
+from sklearn.preprocessing import StandardScaler
+scalar = StandardScaler()
+X_train = scalar.fit_transform (X_train)
+X_test = scalar.fit_transform (X_test) 
+from sklearn.linear_model import LogisticRegression
+LRclassifier = LogisticRegression(C=100,random_state=0)
+LRclassifier.fit(X_train,y_train)
+
+LRpredict = LRclassifier.predict(X_test)
+
+LRclassifier.score(X_train,y_train)
+
+LRclassifier.score(X_test,y_test)
+
+from sklearn.metrics import confusion_matrix
+confusionMatrix = confusion_matrix(y_test,LRpredict)
+
+X = phis.iloc[0:5,[6,14]].values
+y = phis.iloc[0:5,30].values
+
+from sklearn.linear_model import LogisticRegression
+LRclassifier1 = LogisticRegression(C=100,random_state=0)
+LRclassifier1.fit(X_train,y_train)
+
+
+LRpredict1 = LRclassifier1.predict(X_test)
+
+#LRC training score
+LRclassifier1.score(X_train,y_train)
+
+#LRC test score
+LRclassifier1.score(X_test,y_test)
+
+from sklearn.metrics import confusion_matrix
+LRconfusionMatrix1 = confusion_matrix(y_test,LRpredict1)
+
+#visualize the Test set
+xx, yy = np.mgrid[-5:5:.01, -5:5:.01]
+grid = np.c_[xx.ravel(), yy.ravel()]
+probs = LRclassifier1.predict_proba(grid)[:, 1].reshape(xx.shape)
+
+f, ax = plt.subplots(figsize=(8, 6))
+contour = ax.contourf(xx, yy, probs, 25, cmap="RdBu",
+                      vmin=0, vmax=1)
+ax_c = f.colorbar(contour)
+ax_c.set_label("$P(y = 1)$")
+ax_c.set_ticks([0, .25, .5, .75, 1])
+
+ax.scatter(X_test[:, 0], X_test[:, 1],c = (y_test == 1 ), s=50,
+           cmap="RdBu", vmin=-.2, vmax=1.2,
+           edgecolor="white", linewidth=1)
+
+ax.set(aspect="equal",
+       xlim=(-5, 5), ylim=(-5, 5),
+       xlabel="$X_1$", ylabel="$X_2$")
+
+plt.show()
